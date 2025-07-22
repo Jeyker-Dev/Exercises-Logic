a4b91baa8c72169b3f10b1e1b9307dab8d2763ae
Jeyker-Dev <mendozajeyker1178@gmail.com>
2025-07-21 20:51:23 -0400

    Added exercise of palindrome number and funcionality to check if a number is a palindrome

---
+++ LeetCode/Easy/09-palindrome-number.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * Given an integer x, return true if x is a palindrome, and false otherwise.
+
+
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ */
+
+class PalindromeNumber {
+    public function welcomeMessage()
+    {
+        echo "This program will ask you to write a number and tell you if the number is a palindrome number.\n";
+    }
+
+    public function isPalindrome()
+    {
+        echo "Write a number: \n";
+
+        $number = fgets(STDIN);
+
+        $reverseNumber = strrev($number);
+
+        if ($number == $reverseNumber) {
+            echo "The number is a palindrome.";
+        } else {
+            echo "The number is not a palindrome.";
+        }
+    }
+}
+
+$instance = new PalindromeNumber();
+
+$instance->welcomeMessage();
+$instance->isPalindrome();
