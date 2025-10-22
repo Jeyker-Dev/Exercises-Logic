60bbef214a6356c365c029b90b989e1c15aa1396
Jeyker <mendozajeyker1178@gmail.com>
2025-10-22 19:26:33 -0400

    Added Two Sums exercise with functionality to input numbers and target

---
+++ LeetCode/Easy/python/01-two-sums.py
@@ -0,0 +1,59 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+    Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+"""
+from attr.setters import convert
+
+
+class TwoSums:
+    MESSAGE: str = "This program will ask you to write numbers and a target to sum two numbers that you wrote that match the given target."
+
+
+    def __init__(self):
+        self.numbers: list[int] = []
+        self.num: int
+        self.written_number: str | int = ""
+        self.condition: int | None = None
+
+        print("Write the numbers you want: \n Write 0 to exit.")
+
+        while self.condition != 0:
+            self.written_number = input().strip()
+            self.num = self.convert_to_int(self.written_number)
+            self.numbers.append(self.num)
+
+            if self.num == 0:
+                self.condition = self.num
+                self.numbers.remove(self.num)
+
+    @staticmethod
+    def convert_to_int(selection: str) -> int:
+        return int(selection)
+
+TwoSums()
