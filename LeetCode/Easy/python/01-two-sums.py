4e4910cbf7b5dfe4cc621dc890ebbcca518d2c1b
Jeyker <mendozajeyker1178@gmail.com>
2025-10-22 20:31:36 -0400

    Implemented logic to find and display pairs of numbers that sum to the target in Two Sums exercise

--- LeetCode/Easy/python/01-two-sums.py
+++ LeetCode/Easy/python/01-two-sums.py
@@ -40,6 +40,9 @@ class TwoSums:
         self.num: int | None = None
         self.condition: int | None = None
         self.target: int | None = None
+        self.result: int | None = None
+        self.flag: bool = False
+        self.pair: list[int] = []
 
         print("Write the numbers you want: \n Write 0 to exit.")
 
@@ -55,8 +58,29 @@ class TwoSums:
 
         self.target = self.convert_to_int(input().strip())
 
+        for num in self.numbers:
+            for num_2 in self.numbers:
+                self.result = self.sum(num, num_2)
+
+                if self.target == self.result:
+                    self.set_pair_of_target(num, num_2)
+                    self.flag = True
+                    break
+            if self.flag: break
+
+        print("The target is:", self.target, "and the pair of numbers summed was:", self.pair)
+
+
     @staticmethod
     def convert_to_int(selection: str) -> int:
         return int(selection)
+
+    @staticmethod
+    def sum(num: int, num_2: int) -> int:
+        return num + num_2
+
+    def set_pair_of_target(self,num: int, num_2: int):
+        self.pair.append(num)
+        self.pair.append(num_2)
 
 TwoSums()
